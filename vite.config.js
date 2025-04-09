d0d963b718fb150d1003589283cdf4aed6621bb7
BryOk-droid <brianokoth534@gmail.com>
2025-04-09 22:11:55 +0300

    few changes to test functionality of live link

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,8 @@
+// vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/React-App/", // Must match your EXACT repo name
-  build: {
-    outDir: "dist",
-    emptyOutDir: true,
-    rollupOptions: {
-      output: {
-        assetFileNames: "assets/[name].[hash].[ext]",
-        entryFileNames: "assets/[name].[hash].js",
-      },
-    },
-  },
+  base: "/React-App/", // 👈 THIS is critical!
 });
